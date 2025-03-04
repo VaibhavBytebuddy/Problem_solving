2feddc90593310370c1aa3c2133bd1aa20f769bf
VaibhavBytebuddy <om7666bhadane@gmail.com>
2025-03-04 09:24:51 +0530

    this is a my first problem i solve which is 2nd largest element from array .i used TreeSet for this problem , because , if you have this kind of array {3,10,10,10} , so you can not solve using return arr[arr.length-2].So first remove duplicate and sort and then again add in another array and return arr[arr.lenth-2] elemnet this is solution

--- SecLargeEleOfArray.java
+++ SecLargeEleOfArray.java
@@ -6,10 +6,13 @@ import java.util.TreeSet;
 
 public class SecLargeEleOfArray {
 
-    public static void main(String[] args) {
-        int arr[]={3,10,10};
+
+    public int getSecondLargest(int[] arr) {
+        // Code Here
 
         SortedSet<Integer> s=new TreeSet<>();
+
+
 
         for(int i=0;i< arr.length;i++)
         {
@@ -25,7 +28,18 @@ public class SecLargeEleOfArray {
             i++;
         }
 
-        System.out.println(arr2[arr2.length-2]);
+        if(arr2.length==1)
+        {
+            return -1;   //when arr={10,10,10}
+        }
+        return (arr2[arr2.length-2]);
+
+    }
+    public static void main(String[] args) {
+        int arr[]={3,10,10};
+
+       SecLargeEleOfArray a=new SecLargeEleOfArray();
+       a.getSecondLargest(arr);
 
     }
 }

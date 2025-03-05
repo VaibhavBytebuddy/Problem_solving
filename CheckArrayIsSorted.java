9e57089bd6caaaac9174b16c12e017ce6a67a75d
VaibhavBytebuddy <om7666bhadane@gmail.com>
2025-03-05 09:51:08 +0530

    day 2nd of porblem solving , today's problem is check array is sorted or not , so is used a bubble sort method and on flag which check is swap count is zero the make flag is true

---
+++ CheckArrayIsSorted.java
@@ -0,0 +1,64 @@
+package problem_solving;
+
+public class CheckArrayIsSorted {
+    public boolean isSort(int[] arr)
+    {
+        int cmp_cnt=0;
+        int swap_cnt=0;
+
+        int n=arr.length;
+        boolean sorted=false;
+
+        boolean flag=false;
+        for(int i=1;i<n;i++)
+        {
+             flag=false;
+
+            for(int j=0;j<n-i;j++)
+            {
+                cmp_cnt++;
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[i]=temp;
+
+                    swap_cnt++;
+                    flag=true;
+                }
+            }
+            if(flag)
+            {
+
+                System.out.println("Flag value"+flag);
+                break;
+            }
+
+        }
+//        System.out.println(flag);
+
+        System.out.println("No of Comparation "+cmp_cnt);
+        System.out.println("No of swap "+swap_cnt);
+        if(swap_cnt==0)
+        {
+            sorted=true;
+        }
+      return sorted;
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[]={1,2,3,4,5,6};
+        CheckArrayIsSorted a=new CheckArrayIsSorted();
+
+//        System.out.println(a.isSort(arr));
+        if(a.isSort(arr))
+        {
+            System.out.println("Array is already sorted");
+        }
+
+
+    }
+
+}

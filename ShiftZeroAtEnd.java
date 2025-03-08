88cf92868500bee3c79657514f0765b9d4028fb5
VaibhavBytebuddy <om7666bhadane@gmail.com>
2025-03-08 12:09:17 +0530

    There is an different technique where , their is no need of extra list

--- ShiftZeroAtEnd.java
+++ ShiftZeroAtEnd.java
@@ -5,49 +5,84 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ShiftZeroAtEnd {
+       public void shiftZero(int nums[])
+       {
+           if(nums.length==0)
+           {
+               if(nums[0]==0)
+               {
+                   System.out.println(nums[0]);
+               }else
+               {
+                   System.out.println("Their is no zero exist in array");
+               }
+           }
+
+           int zeroCount=0;
+
+           List<Integer> li=new ArrayList<>();
+           for(int i=0;i< nums.length;i++)
+           {
+               if(nums[i]!=0)
+               {
+                   li.add(nums[i]);
+               }else {
+                   zeroCount++;
+               }
+           }
+
+
+
+           while(zeroCount!=0)
+           {
+               li.add(0);
+               zeroCount--;
+           }
+
+           for(int i=0;i< nums.length;i++)
+           {
+               nums[i]=li.get(i);
+           }
+
+           System.out.println(Arrays.toString(nums));
+       }
+
+
+       public void shiftZeroEfficent(int arr[])
+       {
+           int insertPos=0;
+
+           for(int ele:arr)
+           {
+               if(ele!=0)
+               {
+                   arr[insertPos]=ele;
+                   insertPos++;
+               }
+           }
+
+           while(insertPos< arr.length)
+           {
+               arr[insertPos]=0;
+               insertPos++;
+           }
+
+           System.out.println(Arrays.toString(arr));
+       }
+
+
+
     public static void main(String[] args) {
 
-        int[] nums={0,1,0,3,12};
+        int[] nums = {0, 1, 0, 3, 12};
 
-        if(nums.length==0)
-        {
-            if(nums[0]==0)
-            {
-                System.out.println(nums[0]);
-            }else
-            {
-                System.out.println("Their is no zero exist in array");
-            }
-        }
+        ShiftZeroAtEnd s=new ShiftZeroAtEnd();
+        System.out.println("Extra list needed ");
+        s.shiftZero(nums);// efficeint  extra list needed
+        System.out.println("No need to add extra list ");
+        s.shiftZeroEfficent(nums);
 
-        int zeroCount=0;
 
-        List<Integer> li=new ArrayList<>();
-        for(int i=0;i< nums.length;i++)
-        {
-            if(nums[i]!=0)
-            {
-                li.add(nums[i]);
-            }else {
-                zeroCount++;
-            }
-        }
 
-        System.out.println(li.toString());
-
-        System.out.println(zeroCount);
-        while(zeroCount!=0)
-        {
-            li.add(0);
-            zeroCount--;
-        }
-
-        for(int i=0;i< nums.length;i++)
-        {
-            nums[i]=li.get(i);
-        }
-
-        System.out.println(Arrays.toString(nums));
     }
-
 }

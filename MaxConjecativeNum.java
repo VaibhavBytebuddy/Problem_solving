3d428aab54e73ba71264762e233000438792d99c
VaibhavBytebuddy <om7666bhadane@gmail.com>
2025-03-12 23:31:26 +0530

    12 March 2025 i solved count of max conjecative number from array , i counter

---
+++ MaxConjecativeNum.java
@@ -0,0 +1,35 @@
+package problem_solving;
+
+public class MaxConjecativeNum {
+
+    public static void main(String[] args) {
+      // int[] arr={1,1,0,1,1,1};
+        int[] arr={1,0,1,1,0,1};
+
+          int count =0;
+          int temp=1;
+
+          int val=arr[0];
+        for(int i=1;i< arr.length;i++)
+        {
+             if(arr[i]==arr[i-1])
+             {
+                  temp++;
+
+             }else {
+                 if(temp>count)
+                 {
+                     val=arr[i-1];
+                     count=temp;
+                 }
+                 temp=1;
+             }
+
+        }
+
+        System.out.println("Themp"+temp);
+        System.out.println("Value is "+val);
+        System.out.println(count);
+    }
+
+}

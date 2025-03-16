1257c5d850a2709ebf87c29b66e1c6cadde13585
VaibhavBytebuddy <om7666bhadane@gmail.com>
2025-03-16 08:07:02 +0530

    16 march 2025 i solved twosum problem using nested loop, and ask chatgbt,calculate time complexity it is O(n^2) so, chatgbt gives me better approch which is using map

---
+++ TwoSumProblem.java
@@ -0,0 +1,49 @@
+package problem_solving;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TwoSumProblem {
+    public static void main(String[] args) {
+        //2,7,11,15
+        //int[] arr={2, 7, 11, 15};
+        int[] arr={3,2,3};
+        int k=6;
+        int[] res={0,0};
+        for(int i=0;i< arr.length;i++)
+        {
+            for(int j=i+1;j< arr.length;j++)
+            {
+                int sum=arr[i]+arr[j];
+                if(sum==k)
+                {
+                    res[0]=i;
+                    res[1]=j;
+                }
+            }
+
+
+        }
+        System.out.println("This is my approch which take O(n^2) time complexity");
+        System.out.println(Arrays.toString(res));
+
+        System.out.println("This is Better approch because it take O(n) time complexity");
+        int[] i1={0,0};
+        Map<Integer,Integer> m=new HashMap<>();
+        for(int i=0;i< arr.length;i++)
+        {
+            int com=k-arr[i];
+            if(m.containsKey(com))
+            {
+                 i1 = new int[]{m.get(com),i};
+            }
+            m.put(arr[i],i);
+        }
+        System.out.println(Arrays.toString(i1));
+
+
+    }
+}

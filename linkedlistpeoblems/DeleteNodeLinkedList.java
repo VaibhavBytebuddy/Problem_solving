3f254cda3ad3c1a8fd680eda2bce7685d4392992
VaibhavBytebuddy <om7666bhadane@gmail.com>
2025-04-06 06:29:14 +0530

    6-4-25 leetcode problem no 237. Delete Node in a Linked List . Today is solve this problem , i learn in this problem is , some time head line of problem is diff and , decsription has more details, head line cunfuse you.And also check syntax mistack

---
+++ linkedlistpeoblems/DeleteNodeLinkedList.java
@@ -0,0 +1,35 @@
+package problem_solving.linkedlistpeoblems;
+
+
+import static problem_solving.linkedlistpeoblems.ArrayToLinkedList.constructLL;
+import static problem_solving.linkedlistpeoblems.ArrayToLinkedList.displayLL;
+
+public class DeleteNodeLinkedList {
+
+    public  static void deleteNode(Node node)
+    {
+
+        if(node==null || node.next==null)
+        {
+            System.out.println("Node is at last position we can not delete it");
+        }
+
+        node.data=node.next.data;
+        node.next=node.next.next;
+
+    }
+
+
+
+    public static void main(String[] args) {
+        int[] arr={12,1,23,42};
+      Node node=new Node(12);
+
+        Node head=constructLL(arr);
+        displayLL(head);
+
+        deleteNode(node);
+
+
+    }
+}
